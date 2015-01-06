4ed4ebffe5967b6ad217749aeda09cd9c4ac619c
Michael Single <silent.simplay@gmail.com>
2015-01-07 00:37:28 +0100

    Refactored and redocumented PointLight class

--- src/main/java/lightsources/PointLight.java
+++ src/main/java/lightsources/PointLight.java
@@ -1,9 +1,5 @@
 package lightsources;
 
-/**
- * Created by simplaY on 05.01.2015.
- */
-
 import base.HitRecord;
 import base.LightGeometry;
 import base.Ray;
@@ -13,33 +9,53 @@ import materials.PointLightMaterial;
 
 import javax.vecmath.Point3f;
 import javax.vecmath.Vector3f;
-import java.util.Random;
 
 /**
+ * A point light source is a single identifiable localised light source.
+ * Light originates from a single point, and spreads its emission outward in all directions.
  * Implements a point light using a {@link materials.PointLightMaterial}.
+ * Created by simplaY on 05.01.2015.
  */
 public class PointLight implements LightGeometry {
 
-    Vector3f position;
-    PointLightMaterial pointLightMaterial;
-    Random rand;
+    // position of point light source.
+    private final Vector3f position;
 
+    // point light material used for computing the light emission.
+    private final PointLightMaterial pointLightMaterial;
+
+    /**
+     * Construct a point light source by defining its position and its color.
+     *
+     * @param position position of light source
+     * @param emission light emission spectrum
+     */
     public PointLight(Vector3f position, Spectrum emission) {
         this.position = new Vector3f(position);
-        this.rand = new Random();
         pointLightMaterial = new PointLightMaterial(emission);
     }
 
     /**
-     * A ray never hit a point.
+     * There is no intersection when a light hits a point light source
+     *
+     * @param ray intersecting ray
+     * @return HitSentinel since a light source cannot get intersected.
      */
-    public HitRecord intersect(Ray r) {
+    public HitRecord intersect(Ray ray) {
         return HitSentinel.getInstance();
     }
 
     /**
      * Sample a point on the light geometry. On a point light, always return
      * light position with probability one. Set normal to null.
+     */
+
+    /**
+     * Sample a point on the light geometry.
+     * Light position is returned with probability one and normal is set to null.
+     *
+     * @param s samples
+     * @return hit record at sample.
      */
     public HitRecord sample(float[] s) {
         return new HitRecord(new Point3f(position), pointLightMaterial, 1f);
